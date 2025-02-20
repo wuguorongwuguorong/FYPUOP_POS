7876072eb8f4d65f84776fe8111ff7bab5fcb3a2
WuGuorong7854 <daniel.stillpixels@gmail.com>
2025-02-20 16:19:05 +0000

    intital view completed

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+const waxOn = require('wax-on');
+require('dotenv').config();
+const { createConnection } = require('mysql2/promise');
+const { defaultConfiguration } = require('express/lib/application');
+const XLSX = require('xlsx');
+
+let app = express();
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+waxOn.on(hbs.handlebars);
+waxOn.setLayoutPath('./views/layouts');
+
+// Include the 188 handlebar helpers
+const helpers = require('handlebars-helpers')({
+    handlebars: hbs.handlebars
+});
+
+async function main() {
+    // connection = await createConnection({
+    //     'host': process.env.DB_HOST,
+    //     'user': process.env.DB_USER,
+    //     'database': process.env.DB_NAME,
+    //     'password': process.env.DB_PASSWORD
+    // })
+
+    app.get('/', async function (req, res) {
+        res.render('<h1>Hello from Express</h1>');
+
+    });
+}
+
+main();
+
+app.listen(3000, () => {
+    console.log('Server is running')
+});
