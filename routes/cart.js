8817e7a6ea29ffd853bcd08c0fa4cdc6d7e0f456
guorong wu <daniel.stillpixels@gmail.com>
2025-05-13 17:12:41 +0000

    changes in cart route, enabling user to login and select items and add into cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,29 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const cartService = require('../services/cartService');
-const authenticateToken = require('../middlewares/UserAuth');
-
-// Apply the authenticateToken middleware to all routes
-router.use(authenticateToken);
+const AuthenticateWithJWT = require('../middlewares/AuthenticateWithJWT');
 
 // GET cart contents
-router.get('/cart', async (req, res) => {
-    try {
-      const cartContents = await cartService.getCartContents(req.user.userId);
-      res.json(cartContents);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
-  
-  router.put('/editcart', async (req, res) => {
-    try {
-      const cartItems = req.body.cartItems; // Expects an array of items with productId and quantity
-      await cartService.updateCart(req.user.userId, cartItems);
-      res.json({ message: 'Cart updated successfully' });
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  });
+router.get('/', [AuthenticateWithJWT], async (req, res) => {
+  try {
+    const cartContents = await cartService.getCartContents(req.userId);
+    res.json(cartContents);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// PUT bulk update cart
+router.put('/', [AuthenticateWithJWT], async (req, res) => {
+  try {
+    const cartItems = req.body.cartItems; // Expects an array of items with productId and quantity
+    await cartService.updateCart(req.userId, cartItems);
+    res.json({ message: 'Cart updated successfully' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 module.exports = router;
